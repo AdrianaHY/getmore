c08994faba59f94632e02e4ffa925ce45c2071e4
AdrianaHY <adriana.herya@gmail.com>
2017-09-04 19:01:24 -0500

    Función del problema de lógica

---
+++ public/assets/js/logica.js
@@ -0,0 +1,19 @@
+var string = "";
+var stringSeparado = string.split();
+var stringReverso = stringSeparado.reverse();
+
+function buscarLetraRepetida(stringSeparado, stringReverso){
+  for(i=0; i < stringSeparado.length; i++){
+    if(stringSeparado.length[i] == stringReverso[i]){
+      console.log(false);
+    }else(stringSeparado.length[i] != stringReverso[i]){
+      console.log(true)
+    }
+    // for(j = 0; j < (stringSeparado.length-1); i--){
+    //   if(stringSeparado[j] == stringSeparado[j+1]){
+    //
+    //   }
+    }
+  }
+
+}
